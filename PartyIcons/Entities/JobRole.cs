66c31c092c087a840ef2532a980ce3fcb8bdfd4b
HelloWorldOmega <85024604+HelloWorldOmega@users.noreply.github.com>
2021-12-11 09:15:16 +0100

    Split Magical from Ranged
    Another step to do in order to split categories

--- PartyIcons/Entities/JobRole.cs
+++ PartyIcons/Entities/JobRole.cs
@@ -10,6 +10,7 @@ namespace PartyIcons.Entities
         Healer   = 3,
         Crafter  = 4,
         Gatherer = 5,
+        Magical  = 6,
     }
 
     public static class JobRoleExtensions
@@ -21,9 +22,10 @@ namespace PartyIcons.Entities
                 JobRole.Tank     => new[] { Job.GLA, Job.MRD, Job.PLD, Job.WAR, Job.DRK, Job.GNB },
                 JobRole.Healer   => new[] { Job.CNJ, Job.AST, Job.WHM, Job.SCH },
                 JobRole.Melee    => new[] { Job.PGL, Job.LNC, Job.MNK, Job.DRG, Job.ROG, Job.NIN, Job.SAM },
-                JobRole.Ranged   => new[] { Job.ARC, Job.BRD, Job.MCH, Job.DNC, Job.THM, Job.BLM, Job.ACN, Job.SMN, Job.RDM, Job.BLU },
+                JobRole.Ranged   => new[] { Job.ARC, Job.BRD, Job.MCH, Job.DNC },
                 JobRole.Crafter  => new[] { Job.CRP, Job.BSM, Job.ARM, Job.GSM, Job.LTW, Job.WVR, Job.ALC, Job.CUL },
                 JobRole.Gatherer => new[] { Job.MIN, Job.BTN, Job.FSH },
+                JobRole.Magical  => new[] { Job.THM, Job.BLM, Job.ACN, Job.SMN, Job.RDM, Job.BLU },
                 _                => throw new ArgumentException($"Unknown jobRoleID {(int)role}"),
             };
         }
