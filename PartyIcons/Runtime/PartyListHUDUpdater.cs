5e7e482e81992d57517b423671023f7a88fdec21
Vasyl Horbachenko <vasyl.horbachenko@build1.io>
2021-12-27 15:59:42 +0200

    Fix for party list roles reverting back to slot numbers after death

--- PartyIcons/Runtime/PartyListHUDUpdater.cs
+++ PartyIcons/Runtime/PartyListHUDUpdater.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Linq;
 using Dalamud.Game;
+using Dalamud.Game.ClientState;
 using Dalamud.Game.ClientState.Party;
+using Dalamud.Game.Network;
 using Dalamud.IoC;
 using Dalamud.Logging;
 using PartyIcons.Utils;
@@ -12,8 +14,10 @@ namespace PartyIcons.Runtime
     {
         public bool UpdateHUD = false;
 
-        [PluginService] public PartyList PartyList { get; set; }
-        [PluginService] public Framework Framework { get; set; }
+        [PluginService] public PartyList   PartyList   { get; set; }
+        [PluginService] public Framework   Framework   { get; set; }
+        [PluginService] public GameNetwork GameNetwork { get; set; }
+        [PluginService] public ClientState ClientState { get; set; }
 
         private readonly Configuration    _configuration;
         private readonly PartyListHUDView _view;
@@ -30,14 +34,31 @@ namespace PartyIcons.Runtime
 
         public void Enable()
         {
-            _roleTracker.OnAssignedRolesUpdated += OnRolesUpdated;
+            _roleTracker.OnAssignedRolesUpdated += OnAssignedRolesUpdated;
             Framework.Update += OnUpdate;
+            GameNetwork.NetworkMessage += OnNetworkMessage;
         }
 
         public void Dispose()
         {
-            Framework.Update += OnUpdate;
-            _roleTracker.OnAssignedRolesUpdated -= OnRolesUpdated;
+            GameNetwork.NetworkMessage -= OnNetworkMessage;
+            Framework.Update -= OnUpdate;
+            _roleTracker.OnAssignedRolesUpdated -= OnAssignedRolesUpdated;
+        }
+
+        private void OnAssignedRolesUpdated()
+        {
+            PluginLog.Debug("PartyListHUDUpdater forcing update due to assignments update");
+            UpdatePartyListHUD();
+        }
+
+        private void OnNetworkMessage(IntPtr dataptr, ushort opcode, uint sourceactorid, uint targetactorid, NetworkMessageDirection direction)
+        {
+            if (direction == NetworkMessageDirection.ZoneDown && opcode == 0x2ac && targetactorid == ClientState.LocalPlayer?.ObjectId)
+            {
+                PluginLog.Debug("PartyListHUDUpdater Forcing update due to zoning");
+                UpdatePartyListHUD();
+            }
         }
 
         private void OnUpdate(Framework framework)
@@ -52,7 +73,7 @@ namespace PartyIcons.Runtime
             _previousInParty = inParty;
         }
 
-        private void OnRolesUpdated()
+        private void UpdatePartyListHUD()
         {
             if (!_configuration.DisplayRoleInPartyList)
             {
